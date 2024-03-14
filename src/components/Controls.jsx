363856fd1a8ac5d8008bef6002f9a972f1543571
David Andersson <lejonmanen@gmail.com>
2024-03-14 15:03:36 +0100

    Finished v1.0 penguins

--- src/components/Controls.jsx
+++ src/components/Controls.jsx
@@ -1,9 +1,11 @@
 
 
-const Controls = ({ goFishing }) => (
+const Controls = ({ goFishing, hirePenguin, penguinCost, fish }) => (
 	<div>
 		<button onClick={goFishing}> Go fishing </button>
-		<button> Hire penguin (4 fish) </button>
+		<button
+			disabled={fish < penguinCost}
+			onClick={hirePenguin}> Hire penguin ({penguinCost} fish) </button>
 		<button> Hire manager (20 fish) </button>
 		<button disabled> Start over </button>
 	</div>
--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -6,10 +6,19 @@ import Controls from './Controls.jsx'
 const Game = () => {
 	const [fish, setFish] = useState(0)
 	const [penguins, setPenguins] = useState(0)
+	// TODO: lägg till state-variabel för managers
+	const penguinCost = 4
 
 	const goFishing = () => {
-		setFish( fish + 1 )
-		// TODO: lägg till 1 fisk per pingvin
+		setFish( fish + 1 + penguins )
+		// TODO: managers multiplicerar fiskbonus från pingvinerna
+	}
+
+	const hirePenguin = () => {
+		if( fish >= penguinCost ) {
+			setPenguins(penguins + 1)
+			setFish(fish - penguinCost)
+		}
 	}
 
 	return (
@@ -20,7 +29,11 @@ const Game = () => {
 			{/* <!-- Info component --> */}
 			<Info fish={fish} penguins={penguins} />
 			{/* <!-- Controls component --> */}
-			<Controls goFishing={goFishing} />
+			<Controls
+				goFishing={goFishing}
+				hirePenguin={hirePenguin}
+				penguinCost={penguinCost}
+				fish={fish} />
 
 		</section>
 	)
